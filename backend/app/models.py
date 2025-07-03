8da14fafa976b979271ccd653837dcda455aa06c
ShubhamNagar010 <shubhamnagar68819@gmail.com>
2025-07-03 12:35:08 +0530

    seat student relationship update

--- backend/app/models.py
+++ backend/app/models.py
@@ -83,7 +83,7 @@ class Student(Base):
     library_id = Column(Integer, ForeignKey("libraries.id"), nullable=False)
     library = relationship("Library", back_populates="students")
     monthly_payments = relationship("MonthlyPayment", back_populates="student")
-    seat = relationship("Seat", backref="student", uselist=False)
+    seat = relationship("Seat", foreign_keys=[seat_id], backref="student", uselist=False)
 
     
     
