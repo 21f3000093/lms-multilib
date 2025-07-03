cf5d4111d1ddd922670aa7e0e6592f76aee4d821
ShubhamNagar010 <shubhamnagar68819@gmail.com>
2025-07-03 14:43:50 +0530

    remove margin

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,6 +20,13 @@ export default {
 </script>
 
 <style>
+body, html, #app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+
 /* Main wrapper styling */
 .app-background {
   position: relative;
