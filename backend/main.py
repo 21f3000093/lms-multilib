5d4906375cd64479bb6e0b71444e783a06ddaef7
ShubhamNagar010 <shubhamnagar68819@gmail.com>
2025-07-18 17:37:21 +0530

    added custom domain name

--- backend/main.py
+++ backend/main.py
@@ -57,7 +57,7 @@ app.add_middleware(
     CORSMiddleware,
     # allow_origins=[origins],  # Change to frontend URL in production # type: ignore
     # allow_origins=["http://localhost:8080"],  # Change to frontend URL in production
-    allow_origins=["http://localhost:8080","https://lms-virid-three.vercel.app"],  # Change to frontend URL in production
+    allow_origins=["http://localhost:8080","https://lms-virid-three.vercel.app","https://www.smartlibraryapp.in"],  # Change to frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
